48cc2ea41e301a1106eded9d8287d5e83c48ac6b
Jacopo Penazzi <jacopo.penazzi@gmail.com>
2017-01-14 01:16:08 +0100

    testing controller

--- app/js/app.js
+++ app/js/app.js
@@ -4,4 +4,25 @@ testingAngluarApp.controller('testingAngularCtrl', function ($rootScope, $scope)
 
     $scope.title = 'Testing AngularJS Applications';
 
+    $scope.destinations = [];
+    
+    $scope.newDestination = angular.copy(emptyDestination);
+    
+    var emptyDestination = {
+        city: undefined,
+        country: undefined
+    };
+    
+    $scope.addDestination = function() {
+        $scope.destinations.push({
+            city: $scope.newDestination.city,
+            country: $scope.newDestination.country
+        });
+        
+        $scope.newDestination = angular.copy(emptyDestination);
+    };
+    
+    $scope.removeDestination = function(index) {
+        $scope.destinations.splice(index, 1);
+    };
 });
--- app/js/test/unit/testingAngularUnitSpec.js
+++ app/js/test/unit/testingAngularUnitSpec.js
@@ -24,6 +24,58 @@ describe('TestingAngularJS Test Suite', function(){
             
         });
         
+        it('should add 2 destinations to the destination list', function() {
+            expect(scope.destinations).toBeDefined();
+            expect(scope.destinations.length).toBe(0);
+            
+            scope.newDestination = {
+                city: "London",
+                country: "England"
+            };
+            
+            scope.addDestination();
+            
+            expect(scope.destinations.length).toBe(1);
+            expect(scope.destinations[0].city).toBe("London");
+            expect(scope.destinations[0].country).toBe("England");
+            
+            scope.newDestination.city = 'Frankfurt';
+            scope.newDestination.country = 'Germany';
+            
+            scope.addDestination();
+            
+            expect(scope.newDestination.city).toBe(undefined);
+            
+            expect(scope.destinations.length).toBe(2);
+            expect(scope.destinations[1].city).toBe("Frankfurt");
+            expect(scope.destinations[1].country).toBe("Germany");
+            expect(scope.destinations[0].city).toBe("London");
+            expect(scope.destinations[0].country).toBe("England");
+            
+            
+        });
+        
+        it('should remove a destination from the destinations list', function() {
+            scope.destinations = [
+                {
+                    city: 'London',
+                    country: 'England'
+                },
+                {
+                    city: 'Frankfurt',
+                    country: 'Germany'
+                }
+            ];
+            
+            expect(scope.destinations.length).toBe(2);
+                      
+            scope.removeDestination( 0 );
+        
+            expect(scope.destinations.length).toBe(1);
+            expect(scope.destinations[0].city).toBe("Frankfurt");
+            expect(scope.destinations[0].country).toBe("Germany");
+            
+        });
     });
     
 });
